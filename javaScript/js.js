453d8e0b812d890437362a41634de72a09602f0d
leo <leoteste@gmail.com>
2025-08-02 18:10:14 -0300

    animações de pegadas concluidas e tambem foi adicionado o link do meu linkedim, faltam só alguns ajustes de textos e um modal explicando que é um site de portifólio e então o site inicialmente estará pronto

--- javaScript/js.js
+++ javaScript/js.js
@@ -1,4 +1,3 @@
-window.alert('olá, pesso desculpas, mas está pagina ainda está em construção, por favor, volte mais tarde! :)');
 // animação do texto sendo digitado
 const titulo = 'My Universe'
 const textoAnimado = document.getElementById('textoAnimado');
@@ -28,3 +27,4 @@ botao.onclick = () => {
 });
 }
 
+window.alert(' está pagina foi criada com as tecnologias HTML, CSS e JavaScript, e tem como objetivo ser um portifólio de estudos, onde você pode ver o que eu aprendi até agora. Espero que goste!');
